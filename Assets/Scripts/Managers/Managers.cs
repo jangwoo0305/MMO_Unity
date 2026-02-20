20e5d79f5645d7164ef44de63ab66b3b1b6be300
jangwoo0305 <tjwkddn970305@gmail.com>
2026-02-20 18:36:24 +0900

    Singletom Pattern

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Managers :  MonoBehaviour
 {
-    // Start is called before the first frame update
+    static Managers s_instance; // static이라는 특성상 유일성 보장
+
+    public static Managers Instance // 유일한 매니저를 갖고 온다.
+    {
+        get
+        {
+            Init(); 
+            return s_instance;
+        }
+    }
+    
     void Start()
     {
-        
+        Init();
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
+    static void Init()
+    {
+        if (s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject {name = "@Managers"};
+                go.AddComponent<Managers>();
+            }
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
+    }
 }
+ 
