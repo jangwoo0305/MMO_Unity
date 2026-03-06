e5432a240a7b5f3044cb2b83dcb4f742a55b1ff7
jangwoo0305 <tjwkddn970305@gmail.com>
2026-03-06 15:55:53 +0900

    Section5 Raycasting #1

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -30,6 +30,17 @@ public class TestCollision : MonoBehaviour
 
     void Update()
     {
+
+        Vector3 look = transform.TransformDirection(Vector3.forward);
+        Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
+        
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
+        
+        foreach (RaycastHit hit in hits)
+        {
+            Debug.Log($"Raycast{hit.collider.gameObject.name}");
+        }
         
     }
 }
